08c09da247c12ff009f1c211c6bc3dff1cfbd7a7
Harsh Prajapati <harshprajapati2939@gmail.com>
2026-06-09 11:03:10 +0530

    Add files via upload

---
+++ template.js
@@ -0,0 +1,173 @@
+1.
+// let name  = "harsh";
+// console.log(`hello ${name}`);
+
+// 2.
+// let username = "harsh";
+// let course = "uiux design"
+
+// let message = `${username} ${course}` ;
+
+// console.log(message);
+
+// 3.
+//  let num1 = 10;
+//  let num2 = 25;
+
+//  console.log(`${num1 + num2}`);
+ 
+// 4.
+// let mess = `welcome to javascript`;
+// let mess2 = 'Uiux'
+// let mess3 = 'thank you'
+// console.log(`${mess + mess2 + mess3}`);
+
+5.
+// let name = "harsh"
+// let topic = "javascript"
+
+// let concat = name + topic;
+// console.log(concat);
+
+// let template = `${name},${topic}`
+// console.log(template);
+
+// 6.
+// function fun1(user) {
+//     return `welcome back ,${user} `;
+    
+// }
+// let username1 = "harsh";
+// console.log(`${fun1(username1)}`);
+
+
+// 7.
+// let name = "harsh";
+// let age = 22;
+// let city = "palanpur";
+
+// let Profile =`${name},${age},${city}`
+
+// 8.
+// let productname = "laptop ideapad Gaming";
+// let productprice = 69.99;
+
+// console.log(`${productname}${productprice}`);
+
+// 9.
+// let isloggedin = true;
+// let mess = `${isloggedin ? "welcome user": "please login"}`;
+
+// console.log(mess);
+
+// 10.
+
+// let username = "harsh Prajapati";
+
+// let htmlcard = `
+//  <div classs = "card">
+//   <h1>> ${username} </h1>
+//   <p>welcome back </p>
+//   </div>`;
+
+//   console.log(htmlcard);
+
+  
+//   11.
+  
+// let course = "uiux";
+// let itemname = "coding";
+// let amount = 70000;
+
+// let multi = `
+// ${course}
+// ${itemname}
+// ${amount}`;
+
+// console.log(multi);
+
+// 12.
+// let datetime = `current time ${new Date()}`
+// console.log(datetime);
+
+13.
+// let username = "harsh Prajapati";
+
+// let htmlcard = `
+//  <div classs = "card">
+//   <h1>> ${username} </h1>
+//   <p>welcome back </p>
+//   </div>`;
+
+//   console.log(htmlcard);
+
+
+15.
+// let student = "harsh prajapati";
+// let marks = 85;
+// let student2 = "pass";
+
+// let report = `
+// ${student}
+// ${marks}
+// ${student2}`;
+
+// console.log(report);
+
+16.
+// let price = 49.99;
+// let quantity = 85;
+
+// let report = `
+// ${price}
+// ${quantity}`;
+
+// console.log(report);
+
+17.
+
+
+// let employename = "harsh prajapati";
+// let department = "uiux";
+// let id = 12234234;
+
+// let report = `
+// ${employename}
+// ${department}
+// ${id}`;
+
+// console.log(report);
+
+18.
+
+// let companyname = "Excelsior technology";
+// let username = "harsh prajapati";
+// let greeting = "welcome back ";
+
+// let report = `
+// ${companyname}
+// ${username}
+// ${greeting}`;
+
+// console.log(report);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
